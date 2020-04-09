8e5105fe0d6f76f9ad5a8b009112169b50eda7ff
M D <workmixail@gmail.com>
2020-04-09 12:28:30 +0000

    the last of brain games

---
+++ brain_games/games/prime.py
@@ -0,0 +1,25 @@
+"""module contains functions of game even."""
+# coding: utf-8
+
+from operator import mod
+from random import SystemRandom
+
+Description = 'Answer "yes" if number even otherwise answer "no".'
+
+
+def game():
+
+    stop_int = 99
+    dividend = SystemRandom().randint(1, stop_int)
+    divider = 2
+    while divider * divider <= dividend and mod(dividend, divider) != 0:
+        divider += 1
+
+    question = '{0}{1}'.format('Question: ', dividend)
+
+    if divider * divider > dividend:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+
+    return question, correct_answer
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,14 @@
+"""module contains functions of brain-prime script."""
+# coding: utf-8
+
+from brain_games import engine
+from brain_games.games import prime
+
+
+def main():
+
+    engine.run_game(prime)
+
+
+if __name__ == '__main__':
+    main()
